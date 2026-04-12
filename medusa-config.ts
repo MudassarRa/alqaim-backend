814c742c375b99b16ddf50cfb2728865fdf80073
MudassarRa <mudassarraza321@gmail.com>
2026-04-12 22:43:08 +0100

    medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    // Redis connection yahan add karni hai
+    redisUrl: process.env.REDIS_URL, 
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
